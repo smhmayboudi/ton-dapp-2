88238337ead116cbd2fddb3619ce3a42604d4b9b
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-06-10 07:57:20 +0330

    update consts

--- frontend-telegrambot/src/consts/bedrooms.ts
+++ frontend-telegrambot/src/consts/bedrooms.ts
@@ -1,9 +1,9 @@
 import { Metadata } from '../types';
 
-const beds: Metadata[] = [
+const bedrooms: Metadata[] = [
 	{ data: 'master bedroom', text: 'اتاق‌خواب مستر' },
 	{ data: "boy's bedroom", text: 'اتاق‌خواب پسرانه' },
 	{ data: "girl's bedroom", text: 'اتاق‌خواب دخترانه' },
 ];
 
-export default beds;
+export default bedrooms;
--- frontend-telegrambot/src/consts/index.ts
+++ frontend-telegrambot/src/consts/index.ts
@@ -1,7 +1,7 @@
-import beds from './beds';
+import bedrooms from './bedrooms';
 import colour from './colour';
 import meterage from './meterage';
 import rooms from './rooms';
 import styles from './styles';
 
-export default { beds, colour, meterage, rooms, styles };
+export default { bedrooms, colour, meterage, rooms, styles };
